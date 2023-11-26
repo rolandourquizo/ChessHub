e1289e0ec405a1c92796209f0ab6d28d1eb4c0c6
rolandourquizo <151950139+rolandourquizo@users.noreply.github.com>
2023-11-26 11:22:47 -0500

    Add files via upload

---
+++ js/sidebar_function.js
@@ -0,0 +1,9 @@
+function trueSidebar(y) {
+    var x = document.getElementById(y);
+    x.style.display = "block";
+}
+
+function falseSidebar(y) {
+    var x = document.getElementById(y);
+    x.style.display = "none";
+}
